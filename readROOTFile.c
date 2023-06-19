dba486c13d4c98f5dd647e15b9543af1ba9e4caf
Arthur Kraus <akraus3@luc.edu>
2023-06-19 21:08:21 +0200

    updated readROOTFile.c

--- readROOTFile.c
+++ readROOTFile.c
@@ -51,21 +51,17 @@ void readModified()
  
    TFile *f = TFile::Open("hvector_modified.root","READ");
    if (!f) { return; }
-	printf("1\n"); 
    TTree *t; f->GetObject("tvec",t);
    std::vector<float> *vpx_modified = 0; 
-	printf("2\n"); 
    TBranch *bvpx_modified = 0;
    t->SetBranchAddress("vpx_modified",&vpx_modified,&bvpx_modified);
  
-	printf("3\n"); 
    for (Int_t i = 0; i < N; i++) {
       Long64_t tentry = t->LoadTree(i);
       bvpx_modified->GetEntry(tentry);
  
    }
 
-	printf("4\n"); 
    // Since we passed the address of a local variable we need
    // to remove it.
    t->ResetBranchAddresses();
@@ -75,19 +71,14 @@ void readModified()
 int main()
 {   
    gBenchmark = new TBenchmark();
- 	printf("-2\n"); 
    // Timed read()
    gBenchmark->Start("read_hvector");
    read();  
-	printf("-1\n");
    gBenchmark->Show("read_hvector");
  
 
-	printf("0\n"); 
    // Timed readModified();
    gBenchmark->Start("read_hvector_modified");
-   
-	printf("0.1\n"); 
    readModified();
    gBenchmark->Show("read_hvector_modified");
    
